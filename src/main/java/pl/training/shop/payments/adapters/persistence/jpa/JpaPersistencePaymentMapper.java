e23e15e974dd5c73a9157906658c160d445bb41e
landrzejewski <landrzejewski.poczta@gmail.com>
2022-03-29 21:13:11 +0200

    Atomic

--- src/main/java/pl/training/shop/payments/adapters/persistence/jpa/JpaPersistencePaymentMapper.java
+++ src/main/java/pl/training/shop/payments/adapters/persistence/jpa/JpaPersistencePaymentMapper.java
@@ -1,33 +1,17 @@
 package pl.training.shop.payments.adapters.persistence.jpa;
 
-import org.javamoney.moneta.FastMoney;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import pl.training.shop.payments.domain.Payment;
-import pl.training.shop.payments.domain.PaymentStatus;
-
-import java.math.BigDecimal;
 
 @Mapper(componentModel = "spring")
 public interface JpaPersistencePaymentMapper {
 
-    default PaymentEntity toEntity(Payment payment) {
-        var paymentValue = payment.getValue();
-        var entity = new PaymentEntity();
-        entity.setId(payment.getId());
-        entity.setValue(BigDecimal.valueOf(paymentValue.getNumber().doubleValueExact()));
-        entity.setCurrency(paymentValue.getCurrency().getCurrencyCode());
-        entity.setStatus(payment.getStatus().name());
-        entity.setTimestamp(payment.getTimestamp());
-        return entity;
-    }
+    @Mapping(target = "value", expression = "java(java.math.BigDecimal.valueOf(payment.getValue().getNumber().doubleValueExact()))")
+    @Mapping(target = "currency", expression = "java(payment.getValue().getCurrency().getCurrencyCode())")
+    PaymentEntity toEntity(Payment payment);
 
-    default Payment toDomain(PaymentEntity entity) {
-        return Payment.builder()
-                .id(entity.getId())
-                .value(FastMoney.of(entity.getValue(), entity.getCurrency()))
-                .status(PaymentStatus.valueOf(entity.getStatus()))
-                .timestamp(entity.getTimestamp())
-                .build();
-    }
+    @Mapping(target = "value", expression = "java(org.javamoney.moneta.FastMoney.of(entity.getValue(), entity.getCurrency()))")
+    Payment toDomain(PaymentEntity entity);
 
 }
