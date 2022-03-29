29526347935cfcf8bcc87ae1b21bc5a02fa9b823
landrzejewski <landrzejewski.poczta@gmail.com>
2022-03-29 12:59:54 +0200

    Spring

--- src/main/java/pl/training/shop/payments/PaymentsConfiguration.java
+++ src/main/java/pl/training/shop/payments/PaymentsConfiguration.java
@@ -1,7 +1,5 @@
 package pl.training.shop.payments;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -30,7 +28,7 @@ public class PaymentsConfiguration {
     }
 
     @Bean
-    public PaymentProcessor paymentProcessor(/*@Qualifier("uuid")*/ PaymentIdGenerator paymentIdGenerator, TimeService timeService, PaymentRepository paymentRepository) {
+    public PaymentService paymentService(/*@Qualifier("uuid")*/ PaymentIdGenerator paymentIdGenerator, TimeService timeService, PaymentRepository paymentRepository) {
         return new PaymentProcessor(paymentIdGenerator, timeService, paymentRepository);
     }
 
