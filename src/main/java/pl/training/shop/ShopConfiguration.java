9f1c0b6c8a1f1b72999326519ac780c611281324
landrzejewski <landrzejewski.poczta@gmail.com>
2022-03-29 12:18:09 +0200

    Spring

--- src/main/java/pl/training/shop/ShopConfiguration.java
+++ src/main/java/pl/training/shop/ShopConfiguration.java
@@ -1,12 +1,22 @@
 package pl.training.shop;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import pl.training.shop.commons.SystemTimeService;
 import pl.training.shop.commons.TimeService;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@PropertySource("classpath:jdbc.properties")
 @EnableAspectJAutoProxy
 @ComponentScan
 @Configuration
@@ -15,6 +25,39 @@ public class ShopConfiguration {
     @Bean
     public TimeService timeService() {
         return new SystemTimeService();
+    }
+
+    @Bean
+    public DataSource dataSource(Environment environment) {
+        var dataSource = new HikariDataSource();
+        dataSource.setUsername(environment.getProperty("database.username"));
+        dataSource.setPassword(environment.getProperty("database.password"));
+        dataSource.setJdbcUrl(environment.getProperty("database.url"));
+        dataSource.setDriverClassName(environment.getProperty("database.driver"));
+        dataSource.setMaximumPoolSize(20);
+        return dataSource;
+    }
+
+    @Bean
+    public PropertiesFactoryBean jpaProperties() {
+        var factoryBean = new PropertiesFactoryBean();
+        factoryBean.setLocation(new ClassPathResource("jpa.properties"));
+        return factoryBean;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, Properties jpaProperties) {
+        var factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setJpaProperties(jpaProperties);
+        factoryBean.setPackagesToScan("pl.training.shop");
+        factoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        return factoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
     }
 
 }
