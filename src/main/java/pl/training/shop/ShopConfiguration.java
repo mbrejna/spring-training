e007ba097d3931da06370abbebb33218135f59bf
landrzejewski <landrzejewski.poczta@gmail.com>
2022-03-30 15:13:32 +0200

    Atomic

--- src/main/java/pl/training/shop/ShopConfiguration.java
+++ src/main/java/pl/training/shop/ShopConfiguration.java
@@ -1,73 +1,30 @@
 package pl.training.shop;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.*;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.training.shop.time.SystemTimeService;
 import pl.training.shop.time.TimeService;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import java.util.Properties;
-
-@PropertySource("classpath:jdbc.properties")
-@EnableWebMvc
-@EnableJpaRepositories
-@EnableTransactionManagement
-@EnableAspectJAutoProxy
-@ComponentScan
 @Configuration
 public class ShopConfiguration implements WebMvcConfigurer {
 
     @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .build();
+    }
+
+    @Bean
     public TimeService timeService() {
         return new SystemTimeService();
-    }
-
-    @Bean
-    public DataSource dataSource(Environment environment) {
-        var dataSource = new HikariDataSource();
-        dataSource.setUsername(environment.getProperty("database.username"));
-        dataSource.setPassword(environment.getProperty("database.password"));
-        dataSource.setJdbcUrl(environment.getProperty("database.url"));
-        dataSource.setDriverClassName(environment.getProperty("database.driver"));
-        dataSource.setMaximumPoolSize(20);
-        return dataSource;
-    }
-
-    @Bean
-    public PropertiesFactoryBean jpaProperties() {
-        var factoryBean = new PropertiesFactoryBean();
-        factoryBean.setLocation(new ClassPathResource("jpa.properties"));
-        return factoryBean;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, Properties jpaProperties) {
-        var factoryBean = new LocalContainerEntityManagerFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setJpaProperties(jpaProperties);
-        factoryBean.setPackagesToScan("pl.training.shop");
-        factoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
-        return factoryBean;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
     }
 
     @Override
@@ -76,13 +33,6 @@ public class ShopConfiguration implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowedOrigins("http://localhost:4200");
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        var messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        return messageSource;
     }
 
 }
