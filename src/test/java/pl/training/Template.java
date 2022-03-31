22f1fa481dd11785b98c7c4f82585acaf0b64b83
landrzejewski <landrzejewski.poczta@gmail.com>
2022-03-31 13:03:59 +0200

    Atomic

---
+++ src/test/java/pl/training/Template.java
@@ -0,0 +1,52 @@
+package pl.training;
+
+import java.util.Map;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class Template {
+
+    private static final String EXPRESSION_START = "\\$\\{";
+    private static final String EXPRESSION_END = "}";
+    private static final Pattern EXPRESSION = Pattern.compile("\\$\\{\\w+}");
+    private static final String INVALID_VALUE = ".*\\W+.*";
+
+    private final String textWithExpressions;
+
+    public Template(String textWithExpressions) {
+        this.textWithExpressions = textWithExpressions;
+    }
+
+    public String evaluate(Map<String, String> values) {
+        if (isDataValid(values) || isDataComplete(values)) {
+            throw new IllegalArgumentException();
+        }
+        return substitute(values, textWithExpressions);
+    }
+
+    private String substitute(Map<String, String> values, String text) {
+        for (Map.Entry<String, String> entry : values.entrySet()) {
+            var expression = createExpression(entry.getKey());
+            text = text.replaceAll(expression, entry.getValue());
+        }
+        return text;
+    }
+
+    private boolean isDataComplete(Map<String, String> values) {
+        return values.size() != getExpression().count();
+    }
+
+    private Stream<MatchResult> getExpression() {
+        return EXPRESSION.matcher(textWithExpressions).results();
+    }
+
+    private boolean isDataValid(Map<String, String> values) {
+        return values.values().stream().anyMatch(value -> value.matches(INVALID_VALUE));
+    }
+
+    private String createExpression(String key) {
+        return EXPRESSION_START + key + EXPRESSION_END;
+    }
+
+}
