b8e92e29755994b44842ccccbd6df6a53c7b5c92
landrzejewski <landrzejewski.poczta@gmail.com>
2022-03-31 15:16:12 +0200

    Atomic

--- src/main/java/pl/training/shop/Application.java
+++ src/main/java/pl/training/shop/Application.java
@@ -6,21 +6,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pl.training.shop.payments.PaymentRequest;
 import pl.training.shop.payments.PaymentService;
 
-import javax.money.CurrencyUnit;
-import javax.money.Monetary;
-import java.util.Locale;
-
 @Log
 public class Application {
 
-    private static final Locale DEFAULT_LOCALE = new Locale("pl", "PL");
-    private static final CurrencyUnit DEFAULT_CURRENCY_UNIT = Monetary.getCurrency(DEFAULT_LOCALE);
+    private static final String DEFAULT_CURRENCY_CODE = "PLN";
     private static final String CONFIGURATION_FILE = "shop.xml";
 
     public static void main(String[] args) {
         try (var context = new ClassPathXmlApplicationContext(CONFIGURATION_FILE)) {
             var paymentsService = context.getBean(PaymentService.class);
-            var paymentRequest = new PaymentRequest(1L, FastMoney.of(1_000, DEFAULT_CURRENCY_UNIT));
+            var paymentRequest = new PaymentRequest(1L, FastMoney.of(1_000, DEFAULT_CURRENCY_CODE));
             var payment = paymentsService.process(paymentRequest);
             log.info(payment.toString());
         }
