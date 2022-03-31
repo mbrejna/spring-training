3abbfcc238e367a91d35695ed9ff935612753280
landrzejewski <landrzejewski.poczta@gmail.com>
2022-03-31 15:13:17 +0200

    Atomic

--- src/main/java/pl/training/shop/Application.java
+++ src/main/java/pl/training/shop/Application.java
@@ -2,18 +2,13 @@ package pl.training.shop;
 
 import lombok.extern.java.Log;
 import org.javamoney.moneta.FastMoney;
-import pl.training.shop.time.SystemTimeService;
 import pl.training.shop.payments.*;
-
-import javax.money.CurrencyUnit;
-import javax.money.Monetary;
-import java.util.Locale;
+import pl.training.shop.time.SystemTimeService;
 
 @Log
 public class Application {
 
-    private static final Locale DEFAULT_LOCALE = new Locale("pl", "PL");
-    private static final CurrencyUnit DEFAULT_CURRENCY_UNIT = Monetary.getCurrency(DEFAULT_LOCALE);
+    private static final String DEFAULT_CURRENCY_CODE = "PLN";
 
     public static PaymentService paymentService() {
         var paymentIdGenerator = new UUIDPaymentIdGenerator();
@@ -24,7 +19,7 @@ public class Application {
 
     public static void main(String[] args) {
         var paymentsService = paymentService();
-        var paymentRequest = new PaymentRequest(1L, FastMoney.of(1_000, DEFAULT_CURRENCY_UNIT));
+        var paymentRequest = new PaymentRequest(1L, FastMoney.of(1_000, DEFAULT_CURRENCY_CODE));
         var payment = paymentsService.process(paymentRequest);
         log.info(payment.toString());
     }
